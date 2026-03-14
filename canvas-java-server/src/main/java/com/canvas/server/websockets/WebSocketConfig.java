2c8b2590ecbb51f1cf9a9b20e5bfa58ff2ed63ae
Fabio Amarelle <amarellerodriguesfabio@gmail.com>
2026-03-14 22:43:00 +0100

    test

--- canvas-java-server/src/main/java/com/canvas/server/websockets/WebSocketConfig.java
+++ canvas-java-server/src/main/java/com/canvas/server/websockets/WebSocketConfig.java
@@ -1,10 +1,11 @@
 package com.canvas.server.websockets;
 
-import com.canvas.server.WhiteboardWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.canvas.server.WhiteboardWebSocketHandler;
 
 @Configuration
 @EnableWebSocket
@@ -18,8 +19,11 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-    registry.addHandler(whiteboardWebSocketHandler, "/ws/whiteboard/**") 
-            .setAllowedOriginPatterns("*")
-            .withSockJS();
+        registry.addHandler(whiteboardWebSocketHandler, "/ws/whiteboard/{id}") 
+            .setAllowedOrigins(
+                "https://canvas.fabioamarelle.com", 
+                "http://localhost:5173", 
+                "http://localhost"
+            );
     }
 }
